e7c2626fe9e38a086daf5e0ef57608ebc76acdb9
lzx3778 <1249574830@qq.com>
2016-10-15 13:15:55 +0800

    new matrix calculate

--- src/math/Matrix.java
+++ src/math/Matrix.java
@@ -1,57 +1,17 @@
-package math;
+import MatrixGenerator.*;
 
-public class Matrix {
-
-	public static void main(String[] args) {
-		int[][] m1 = new int[2][3];
-		int[][] m2 = new int[3][2];
-		//m1 initiallize
-		for (int i = 0; i < m1.length; i++) {
-			for (int j = 0; j < m1[1].length; j++) {
-				m1[i][j] = i * j;
-				System.out.print(i * j + " ");
-			}
-			System.out.println("");
-		}
-		System.out.println("");
-		//m2 initiallize
-		for (int i = 0; i < m2.length; i++) {
-			for (int j = 0; j < m2[1].length; j++) {
-				m2[i][j] = i * j;
-				System.out.print(i * j + " ");
-			}
-			System.out.println("");
-		}
-		System.out.println("");
-		//check if allow
-		if (M_x_M(m1, m2) != null) {
-			//get matrix ans
-			int[][] Matrix = M_x_M(m1, m2);
-			//print matrix
-			for (int i = 0; i < Matrix.length; i++) {
-				for (int j = 0; j < Matrix.length; j++) {
-					System.out.print(Matrix[i][j] + " ");
-				}
-				System.out.println("");
-			}
-		} else {
-			System.out.println("invalid");
-		}
-
-	}
-
-	public static int[][] M_x_M(int[][] i1, int[][] i2) {
-		// i1 width == i2 lendth
-		if (i1[1].length == i2.length) {
-			int[][] ans = new int[i1[1].length][i2.length];
-			for (int i = 0; i < i1[1].length; i++) {
-				for (int j = 0; j < i2.length; j++) {
-					ans[j][i] = i1[i][j] * i2[j][i];
-					System.out.println(ans[j][i]);
-				}
-			}
-			return ans;
-		}
-		return null;
-	}
+public class Matrix{
+	private MatrixGenerator matrix = new MatrixGenerator();
+    public static void main(String[] args) {
+        int[][] ans = 2DM(matrix.random2DMatrix(0,3,2),matrix.random2DMatrix(0,2,3));
+    }
+    public static int[][] 2DM(int[][] i1,int[][] i2){
+        int[][] i3 = new int[i1.length][i2[1].length];
+        for(int i = 0;i < i1.length;i ++){
+            for(int j = 0;j < i2[i].length;j ++){
+                i3[i][j] = i1[i][j] * i2[i][j] + i1[i][j + 1] + i2[i + 1][j];
+            }
+        }
+        return i3;
+    }
 }
---
+++ src/math/MatrixGenerator.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class MatrixGenerator{
+    public static int[][] random2DMatrix(int seed,int width,int length){
+		Random r = new Random(seed);
+        int[][] i1 = new int[length][width];
+        for(int i = 0;i < length;i ++){
+            for(int j = 0;j < width;j ++){
+                i1[i][j] = r.nextInt();
+            }
+        }
+		return i1;
+    }
+}
