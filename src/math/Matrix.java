793843a86b0060f217633882e94d44c71d6df204
lzx3778 <1249574830@qq.com>
2016-10-15 15:14:31 +0800

    2016-10-15
    corrected some errors(still not perfect)

--- src/math/Matrix.java
+++ src/math/Matrix.java
@@ -1,14 +1,35 @@
 package math;
 
 public class Matrix{
-	private MatrixGenerator matrix = new MatrixGenerator();
-    public static void main(String[] args) {
-        int[][] ans = 2DM(matrix.random2DMatrix(0,3,2),matrix.random2DMatrix(0,2,3));
+	public static void main(String[] args) {
+    	int[][] m1 = MatrixGenerator.random2DMatrix(3,2);
+    	int[][] m2 = MatrixGenerator.random2DMatrix(2,3);
+        int[][] ans = TwoDMatrix(m1,m2);
+        for(int i = 0;i < m1.length;i ++){
+            for(int j = 0;j < m1[1].length;j ++){
+                System.out.print(m1[i][j]+" ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        for(int i = 0;i < m2.length;i ++){
+            for(int j = 0;j < m2[1].length;j ++){
+                System.out.print(m2[i][j]+" ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        for(int i = 0;i < ans.length;i ++){
+            for(int j = 0;j < ans[1].length;j ++){
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println("");
+        }
     }
-    public static int[][] 2DM(int[][] i1,int[][] i2){
+    public static int[][] TwoDMatrix(int[][] i1,int[][] i2){
         int[][] i3 = new int[i1.length][i2[1].length];
-        for(int i = 0;i < i1.length;i ++){
-            for(int j = 0;j < i2[i].length;j ++){
+        for(int i = 0;i < i3.length-1;i ++){
+            for(int j = 0;j < i3[i].length-1;j ++){
                 i3[i][j] = i1[i][j] * i2[i][j] + i1[i][j + 1] + i2[i + 1][j];
             }
         }
--- src/math/MatrixGenerator.java
+++ src/math/MatrixGenerator.java
@@ -1,14 +1,11 @@
 package math;
 
-import java.util.Random;
-
 public class MatrixGenerator{
-    public static int[][] random2DMatrix(int seed,int width,int length){
-		Random r = new Random(seed);
+    public static int[][] random2DMatrix(int width,int length){
         int[][] i1 = new int[length][width];
         for(int i = 0;i < length;i ++){
             for(int j = 0;j < width;j ++){
-                i1[i][j] = r.nextInt();
+                i1[i][j] = i+j;
             }
         }
 		return i1;
