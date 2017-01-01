271f6a6f7faaad944ab7fd788547b480642f1872
lzx3778 <1249574830@qq.com>
2017-01-01 16:27:20 +0800

    the using of algs4

--- src/mathGraph/G1.java
+++ src/mathGraph/G1.java
@@ -3,15 +3,15 @@ package mathGraph;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class G1 {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int N = 100;
 		StdDraw.setXscale(0, N);
-		StdDraw.setYscale(0, N*N);
+		StdDraw.setYscale(0, N * N);
 		StdDraw.setPenRadius(.01);
-		for(int i = 1;i <= N; i++){
+		for (int i = 1; i <= N; i++) {
 			StdDraw.point(i, i);
-			StdDraw.point(i, i*i);
-			StdDraw.point(i, i*Math.log(i));
+			StdDraw.point(i, i * i);
+			StdDraw.point(i, i * Math.log(i));
 		}
 	}
 }
--- src/mathGraph/G2.java
+++ src/mathGraph/G2.java
@@ -7,13 +7,13 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class G2 {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int N = 50;
 		double[] a = new double[N];
-		for(int i = 0;i < N;i ++){
+		for (int i = 0; i < N; i++) {
 			a[i] = StdRandom.random();
 		}
-		for(int i = 0;i < N;i ++){
+		for (int i = 0; i < N; i++) {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
@@ -21,9 +21,9 @@ public class G2 {
 				e.printStackTrace();
 			}
 			double x = 1.0 * i / N;
-			double y = a[i]/2.0;
-			double rw = 0.5/N;
-			double rh = a[i]/2.0;
+			double y = a[i] / 2.0;
+			double rw = 0.5 / N;
+			double rh = a[i] / 2.0;
 			StdDraw.filledRectangle(x, y, rw, rh);
 		}
 		try {
@@ -32,7 +32,7 @@ public class G2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for(int i = 0;i < N;i ++){
+		for (int i = 0; i < N; i++) {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
@@ -40,15 +40,15 @@ public class G2 {
 				e.printStackTrace();
 			}
 			double x = 1.0 * i / N;
-			double y = a[i]/2.0;
-			double rw = 0.5/N;
-			double rh = a[i]/2.0;
+			double y = a[i] / 2.0;
+			double rw = 0.5 / N;
+			double rh = a[i] / 2.0;
 			StdDraw.setPenColor(Color.WHITE);
 			StdDraw.filledRectangle(x, y, rw, rh);
 		}
 		Arrays.sort(a);
 		StdDraw.setPenColor(Color.BLACK);
-		for(int i = 0;i < N;i ++){
+		for (int i = 0; i < N; i++) {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
@@ -56,9 +56,9 @@ public class G2 {
 				e.printStackTrace();
 			}
 			double x = 1.0 * i / N;
-			double y = a[i]/2.0;
-			double rw = 0.5/N;
-			double rh = a[i]/2.0;
+			double y = a[i] / 2.0;
+			double rw = 0.5 / N;
+			double rh = a[i] / 2.0;
 			StdDraw.filledRectangle(x, y, rw, rh);
 		}
 	}
